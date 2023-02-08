1738aa2a54ee76b79a9d81028e04a742a2947706
avivashishta29 <avivashishta@gmail.com>
2023-02-08 16:23:26 +0530

    students arr and function name change

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 /** @format */
 
 let arr = [
-  { id: 1, name: "john", age: "18", profession: "developer" },
-  { id: 2, name: "jack", age: "20", profession: "developer" },
-  { id: 3, name: "karen", age: "19", profession: "admin" },
+  { id: 1, name: "john", age: "18", marks: 80 },
+  { id: 2, name: "jack", age: "20", marks: 85 },
+  { id: 3, name: "karen", age: "19", marks: 35 },
 ];
 
-function PrintDeveloperswithMap() {
-  //Write your code here
+function PrintStudentswithMap() {
+  //Write your code here , just console.log
 }
 
-function PrintDeveloperbyForEach() {
-  //Write your code here
+function PrintStudentsbyForEach() {
+  //Write your code here , just console.log
 }
 
 function addData() {
-  //Write your code here
+  //Write your code here, just console.log
 }
 
-function removeAdmin() {
-  //Write your code here
+function removeFailedStudent() {
+  //Write your code here, just console.log
 }
 
 function concatenateArray() {
-  //Write your code here
+  //Write your code here, just console.log
 }
